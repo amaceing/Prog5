f885b1c8dc30b6fe2850abd0ab50f828c6e988d6
Anthony Mace <amace93@gmail.com>
2013-10-16 00:12:26 -0700

    implements RectangularBox class

--- src/program5.java
+++ src/program5.java
@@ -8,7 +8,16 @@ public class program5 {
     Scanner console = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Implement later");
+        Cylinder test = new Cylinder(5, 4, 3, 2);
+        System.out.println(test);
+        RectangularBox test1 = new RectangularBox(5, 4, 3, 2, 1);
+        System.out.println(test1);
+        Container[] collect = new Container[2];
+        collect[0] = test;
+        collect[1] = test1;
+        for (int i = 0; i < collect.length; i++) {
+            System.out.println(collect[i]);
+        }
     }
 
 }
@@ -44,7 +53,7 @@ abstract class Container {
 
     public String toString() {
         return getClass() + "\n" +
-               "Wall thickness (in.): " + _wallThickness + "\n" +
+               "Wall thickness: " + _wallThickness + "\n" +
                "Wall density: " + _wallDensity + "\n";
     }
 }
@@ -84,6 +93,53 @@ class Cylinder extends Container {
     public String toString() {
         return super.toString() +
                "Radius: " + _radius + "\n" +
+               "Length: " + _length + "\n";
+    }
+}
+
+class RectangularBox extends Container {
+    private double _height;
+    private double _width;
+    private double _length;
+
+    public RectangularBox(double wallThickness, double wallDensity, double height,
+                          double width, double length) {
+        super(wallThickness, wallDensity);
+        _height = height;
+        _width = width;
+        _length = length;
+    }
+
+    public double getHeight() {
+        return _height;
+    }
+
+    public double getWidth() {
+        return _width;
+    }
+
+    public double getLength() {
+        return _length;
+    }
+
+    public double computeExteriorVolume() {
+        double extVol = 0.0;
+        extVol = _height * _width * _length;
+        return extVol;
+    }
+
+    public double computeInteriorVolume() {
+        double intVol = 0.0;
+        intVol = (_height * (2 * getWallThickness())) *
+                 (_width * (2 * getWallThickness())) *
+                 (_length * (2 * getWallThickness()));
+        return intVol;
+    }
+
+    public String toString() {
+        return super.toString() +
+               "Height: " + _height + "\n" +
+               "Width: " + _width + "\n" +
                "Length: " + _length + "\n";
     }
 }
